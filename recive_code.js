b33b870f7373acf3cbc7befd7d29c8c121337269
sato2004 <voloshenkoa@gmail.com>
2015-10-15 15:49:22 +0300

    Create recive_code.js

---
+++ recive_code.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+			
+                document.addEventListener("scanner", function(e) { 
+				
+                    document.getElementById("rf_id").value = e.detail.barcode;
+                });
+            </script>
+
+</head>
